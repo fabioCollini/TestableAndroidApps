f289bd35a85558200eb4658f17e00d947fa47444
Fabio Collini <fabio.collini@gmail.com>
2014-10-04 12:21:04 +0200

    Moved start code

--- mvc/src/main/java/it/cosenonjaviste/testableandroidapps/mvc/RepoListController.java
+++ mvc/src/main/java/it/cosenonjaviste/testableandroidapps/mvc/RepoListController.java
@@ -23,40 +23,40 @@ public class RepoListController extends RxMvcController<RepoListModel> {
     }
 
     public void listRepos(String queryString) {
+        model.setProgressVisible(true);
+        model.setReloadVisible(false);
+        notifyModelChanged();
         repoService.listRepos(contextBinder, queryString);
     }
 
     private Subscription subscribeRepoList() {
-        return repoService.getLoadQueue().subscribe((item, observable) -> {
-            model.setProgressVisible(true);
-            model.setReloadVisible(false);
-            notifyModelChanged();
-            return observable.subscribe(model::setRepos, throwable -> {
-                model.setProgressVisible(false);
-                model.setReloadVisible(true);
-                notifyModelChanged();
-            }, () -> {
-                model.setProgressVisible(false);
-                notifyModelChanged();
-            });
-        });
+        return repoService.getLoadQueue().subscribe((item, observable) ->
+                        observable.subscribe(model::setRepos, throwable -> {
+                            model.setProgressVisible(false);
+                            model.setReloadVisible(true);
+                            notifyModelChanged();
+                        }, () -> {
+                            model.setProgressVisible(false);
+                            notifyModelChanged();
+                        })
+        );
     }
 
     private Subscription subscribeRepo() {
-        return repoService.getRepoQueue().subscribe((item, observable) -> {
-            model.getUpdatingRepos().add(item.getId());
-            notifyModelChanged();
-            return observable
-                    .finallyDo(() -> model.getUpdatingRepos().remove(item.getId()))
-                    .subscribe(repo -> {
-                    }, e -> {
-                        model.setExceptionMessage(e.getMessage());
-                        notifyModelChanged();
-                    }, this::notifyModelChanged);
-        });
+        return repoService.getRepoQueue().subscribe(
+                (item, observable) ->
+                        observable.finallyDo(() -> model.getUpdatingRepos().remove(item.getId()))
+                                .subscribe(repo -> {
+                                }, e -> {
+                                    model.setExceptionMessage(e.getMessage());
+                                    notifyModelChanged();
+                                }, () -> notifyModelChanged())
+        );
     }
 
     public void toggleStar(Repo repo) {
+        model.getUpdatingRepos().add(repo.getId());
+        notifyModelChanged();
         repoService.toggleStar(contextBinder, repo);
     }
 
