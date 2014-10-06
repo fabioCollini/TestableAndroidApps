440d6c556b30246b1f7ea3a022e275d99c367976
Fabio Collini <fabio.collini@gmail.com>
2014-10-06 08:26:22 +0200

    Test using TestScheduler

--- mvc/src/test/java/it/cosenonjaviste/testableandroidapps/mvc/pausable/PausableTest.java
+++ mvc/src/test/java/it/cosenonjaviste/testableandroidapps/mvc/pausable/PausableTest.java
@@ -12,6 +12,8 @@ import it.cosenonjaviste.testableandroidapps.mvc.base.PausableSubscriptions;
 import rx.Observable;
 import rx.functions.Action1;
 import rx.observers.Observers;
+import rx.schedulers.Schedulers;
+import rx.schedulers.TestScheduler;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,74 +22,88 @@ import static org.junit.Assert.assertEquals;
  */
 public class PausableTest {
     @Test public void testPauseResume() {
+        TestScheduler testScheduler = Schedulers.test();
         List<Long> list = new ArrayList<>();
-        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS).limit(5);
+        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS, testScheduler).limit(5);
         PausableSubscription pausableSubscription = PausableSubscriptions.subscribe(observable, Observers.create((Action1<Long>) list::add));
-        sleep(800);
+        sleep(testScheduler, 800);
         pausableSubscription.pause();
         pausableSubscription.resume();
-        sleep(2000);
+        sleep(testScheduler, 2000);
+        assertEquals(Arrays.asList(0L, 1L, 2L, 3L, 4L), list);
+    }
+
+    @Test public void testPauseCompletedResume() {
+        TestScheduler testScheduler = Schedulers.test();
+        List<Long> list = new ArrayList<>();
+        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS, testScheduler).limit(5);
+        PausableSubscription pausableSubscription = PausableSubscriptions.subscribe(observable, Observers.create((Action1<Long>) list::add));
+        sleep(testScheduler, 800);
+        pausableSubscription.pause();
+        sleep(testScheduler, 2000);
+        pausableSubscription.resume();
         assertEquals(Arrays.asList(0L, 1L, 2L, 3L, 4L), list);
     }
 
     @Test public void testPauseSleepResume() {
+        TestScheduler testScheduler = Schedulers.test();
         List<Long> list = new ArrayList<>();
-        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS).limit(5);
+        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS, testScheduler).limit(5);
         PausableSubscription pausableSubscription = PausableSubscriptions.subscribe(observable, Observers.create((Action1<Long>) list::add));
-        sleep(800);
+        sleep(testScheduler, 800);
         pausableSubscription.pause();
-        sleep(900);
+        sleep(testScheduler, 900);
         pausableSubscription.resume();
-        sleep(1000);
+        sleep(testScheduler, 1000);
         assertEquals(Arrays.asList(0L, 1L, 2L, 3L, 4L), list);
     }
 
     @Test public void testReplay() {
+        TestScheduler testScheduler = Schedulers.test();
         List<Long> list1 = new ArrayList<>();
         List<Long> list2 = new ArrayList<>();
-        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS).limit(5).replay().refCount();
+        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS, testScheduler).limit(5).replay().refCount();
         PausableSubscription pausableSubscription1 = PausableSubscriptions.subscribe(observable, Observers.create((Action1<Long>) list1::add));
         PausableSubscription pausableSubscription2 = PausableSubscriptions.subscribe(observable, Observers.create((Action1<Long>) list2::add));
-        sleep(800);
+        sleep(testScheduler, 800);
         pausableSubscription1.pause();
-        sleep(900);
+        sleep(testScheduler, 900);
         pausableSubscription1.resume();
-        sleep(1000);
+        sleep(testScheduler, 1000);
         assertEquals(Arrays.asList(0L, 1L, 2L, 3L, 4L), list1);
         assertEquals(Arrays.asList(0L, 1L, 2L, 3L, 4L), list2);
     }
 
     @Test public void testReplayDestroy() {
+        TestScheduler testScheduler = Schedulers.test();
         List<Long> list1 = new ArrayList<>();
         List<Long> list2 = new ArrayList<>();
-        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS).limit(5).replay().refCount();
+        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS, testScheduler).limit(5).replay().refCount();
         PausableSubscription pausableSubscription1 = PausableSubscriptions.subscribe(observable, Observers.create((Action1<Long>) list1::add));
         PausableSubscription pausableSubscription2 = PausableSubscriptions.subscribe(observable, Observers.create((Action1<Long>) list2::add));
-        sleep(800);
+        sleep(testScheduler, 800);
         pausableSubscription1.destroy();
-        sleep(900);
+        sleep(testScheduler, 900);
         pausableSubscription2.pause();
         pausableSubscription2.resume();
-        sleep(1000);
+        sleep(testScheduler, 1000);
         assertEquals(Arrays.asList(0L), list1);
         assertEquals(Arrays.asList(0L, 1L, 2L, 3L, 4L), list2);
     }
 
     @Test public void testDestroy() {
+        TestScheduler testScheduler = Schedulers.test();
         List<Long> list = new ArrayList<>();
-        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS).limit(5);
+        Observable<Long> observable = Observable.interval(500, TimeUnit.MILLISECONDS, testScheduler).limit(5);
         PausableSubscription pausableSubscription = PausableSubscriptions.subscribe(observable, Observers.create((Action1<Long>) list::add));
-        sleep(800);
+        sleep(testScheduler, 800);
         pausableSubscription.destroy();
-        sleep(2000);
+        sleep(testScheduler, 2000);
         assertEquals(Arrays.asList(0L), list);
     }
 
-    private void sleep(int i) {
-        try {
-            Thread.sleep(i);
-        } catch (InterruptedException ignored) {
-        }
+    private void sleep(TestScheduler testScheduler, int i) {
+        testScheduler.advanceTimeBy(i, TimeUnit.MILLISECONDS);
     }
 
 }
